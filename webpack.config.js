fa4fe500201a78038f87c31a2012f660e9a9def6
Shaq <shaqdulove@hotmail.com>
2020-05-18 15:35:31 -0400

    setup dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/app.js",
   output: {
-    path: path.join(__dirname, "public"),
+    path: path.join(__dirname, "public/dist"),
     filename: "bundle.js",
   },
   module: {
@@ -23,6 +23,7 @@ module.exports = {
   devtool: "eval-cheap-module-source-map",
   devServer: {
     contentBase: path.join(__dirname, "public"),
+    publicPath: "/dist/",
     port: 5000,
     historyApiFallback: true,
   },
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: "production",
   entry: "./src/app.js",
   output: {
-    path: path.join(__dirname, "public"),
+    path: path.join(__dirname, "public/dist"),
     filename: "bundle.js",
   },
   module: {
@@ -23,11 +23,6 @@ module.exports = {
     ],
   },
   plugins: [new miniCssExtractPlugin({ filename: "style.css" })],
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    port: 3000,
-    historyApiFallback: true,
-  },
 };
 
 //loader
