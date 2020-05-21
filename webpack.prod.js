81b954c8ccb0123316abce1c6800c03ce6fd41c2
Shaq <shaqdulove@hotmail.com>
2020-05-21 19:30:55 -0400

    .

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 // entry -> output
 const path = require("path");
-const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
 const miniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -23,20 +23,7 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new miniCssExtractPlugin({ filename: "style.css" }),
-    new webpack.DefinePlugin({
-      "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY),
-      "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-      "process.env.FIREBASE_DATABASE_URL": JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-      "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-      "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-      "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
-        process.env.FIREBASE_MESSAGING_SENDER_ID
-      ),
-      "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.FIREBASE_APP_ID),
-    }),
-  ],
+  plugins: [new Dotenv({ systemvars: true }), new miniCssExtractPlugin({ filename: "style.css" })],
 };
 
 //loader
